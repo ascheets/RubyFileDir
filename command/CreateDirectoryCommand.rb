819e4641190747ce83a7c583a0586b30f7626a04
AaronScheets <ascheets@carthage.edu>
2016-02-29 14:55:07 -0600

    preemptive commit

--- command/CreateDirectoryCommand.rb
+++ command/CreateDirectoryCommand.rb
@@ -3,7 +3,29 @@ require_relative 'DeleteDirectoryCommand'
 
 class CreateDirectoryCommand < Command
 
-  def initialize
+  def initialize(p)
+
+    self.description = "Creates the directory: #{p}"
+
+    #store path
+    self.path = p
+
+  end
+
+  def execute
+    
+    if !Dir.exist? path
+      
+      Dir.mkdir(path)
+
+    end
+
+  end
+
+  def undo
+
+    c = DeleteDirectoryCommand.new(path)
+    c.execute
 
   end
 
